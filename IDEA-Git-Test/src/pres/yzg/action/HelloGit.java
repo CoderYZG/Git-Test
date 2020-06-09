69e532852a3a6a8872fce331a3a52cde6a1668b5
Yin <yinzhigang1024@163.com>
2020-06-09 21:02:40 +0800

    将test工程提交给Git

---
+++ IDEA-Git-Test/src/pres/yzg/action/HelloGit.java
@@ -0,0 +1,12 @@
+package pres.yzg.action;
+
+/**
+ * @author 尹志刚
+ * @date 2020/6/9 20:36
+ */
+public class HelloGit {
+  public static void main(String[] args) {
+    //
+    System.out.println("Hello Git!");
+  }
+}
---
+++ IDEA-Git-Test/src/pres/yzg/pojo/Person.java
@@ -0,0 +1,14 @@
+package pres.yzg.pojo;
+
+import java.util.Date;
+
+/**
+ * @author 尹志刚
+ * @date 2020/6/9 20:33
+ */
+public class Person {
+    private Integer IDCard;
+    private String name;
+    private Character sex;
+    private Date brithday;
+}
